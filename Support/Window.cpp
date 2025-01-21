e209f46fcc5da11e701764826d3e6178acf3f7bb
Mathieu OUVRARD <114410207+MathieuOUVRARD-Professional@users.noreply.github.com>
2025-01-21 15:41:23 +0100

    RTV & Descriptor Heap

--- Support/Window.cpp
+++ Support/Window.cpp
@@ -85,6 +85,27 @@ bool DXWindow::Init()
 		return false;
 	}	
 
+	//Create RTV Heap
+	D3D12_DESCRIPTOR_HEAP_DESC descHeapDesc{};
+	descHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	descHeapDesc.NumDescriptors = FrameCount;
+	descHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	descHeapDesc.NodeMask = 0;
+
+	if (FAILED(DXContext::Get().GetDevice()->CreateDescriptorHeap(&descHeapDesc, IID_PPV_ARGS(&m_rtvDescHeap))))
+	{
+		return false;
+	}
+	
+	//Create handles to view
+	auto firstHandle = m_rtvDescHeap->GetCPUDescriptorHandleForHeapStart();
+	auto handleIncrement = DXContext::Get().GetDevice()->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	for (size_t i = 0; i < FrameCount; ++i)
+	{
+		m_rtvHandles[i] = firstHandle;
+		m_rtvHandles[i].ptr += handleIncrement * i;
+	}	
+
 	//Get Buffers
 	if (!GetBuffers())
 	{
@@ -170,6 +191,8 @@ void DXWindow::Shutdown()
 {
 	ReleaseBuffers();
 
+	m_rtvDescHeap.Release();
+
 	m_swapChain.Release();
 
 	if (m_window)
@@ -196,6 +219,11 @@ void DXWindow::BeginFrame(ID3D12GraphicsCommandList* cmdList)
 	barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
 
 	cmdList->ResourceBarrier(1, &barrier);
+
+	float clearColor[] = { 0.9f, 0.4f, 0.f, 1.f };
+	cmdList->ClearRenderTargetView(m_rtvHandles[m_currentBufferIndex], clearColor, 0, nullptr);
+
+	cmdList->OMSetRenderTargets(1, &m_rtvHandles[m_currentBufferIndex], false, nullptr);
 }
 
 void DXWindow::EndFrame(ID3D12GraphicsCommandList* cmdList)
@@ -220,6 +248,13 @@ bool DXWindow::GetBuffers()
 		{
 			return false;
 		}
+
+		D3D12_RENDER_TARGET_VIEW_DESC rtv{};
+		rtv.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		rtv.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+		rtv.Texture2D.MipSlice = 0;
+		rtv.Texture2D.PlaneSlice = 0;
+		DXContext::Get().GetDevice()->CreateRenderTargetView(m_buffers[i], &rtv, m_rtvHandles[i]);
 	}
 
 	return true;
