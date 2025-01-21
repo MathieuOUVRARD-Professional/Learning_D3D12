2115ae2fe701a9db655199ee4d3541109dad279d
Mathieu OUVRARD <114410207+MathieuOUVRARD-Professional@users.noreply.github.com>
2025-01-21 18:36:04 +0100

    Resources, heaps and copy buffer to GPU

--- main.cpp
+++ main.cpp
@@ -14,8 +14,59 @@ int main()
 
 	if (DXContext::Get().Init() && DXWindow::Get().Init())
 	{
-		DXWindow::Get().SetFullscreen(true);
+		const char* hello = "Hello World!";
 
+		//Upload heap CPU --> GPU
+		D3D12_HEAP_PROPERTIES uploadHeapProperties{};
+		uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;
+		uploadHeapProperties.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+		uploadHeapProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+		uploadHeapProperties.CreationNodeMask = 0;
+		uploadHeapProperties.VisibleNodeMask = 0;
+
+		//Default heap, GPU only
+		D3D12_HEAP_PROPERTIES defaultHeapProperties{};
+		defaultHeapProperties.Type = D3D12_HEAP_TYPE_DEFAULT;
+		defaultHeapProperties.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+		defaultHeapProperties.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+		defaultHeapProperties.CreationNodeMask = 0;
+		defaultHeapProperties.VisibleNodeMask = 0;
+
+		//Upload buffer
+		D3D12_RESOURCE_DESC resourceDescriptor{};
+		resourceDescriptor.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		resourceDescriptor.Alignment = D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT;
+		resourceDescriptor.Width = 1024;
+		resourceDescriptor.Height = 1;
+		resourceDescriptor.DepthOrArraySize = 1;
+		resourceDescriptor.MipLevels = 1;
+		resourceDescriptor.Format = DXGI_FORMAT_UNKNOWN;
+		resourceDescriptor.SampleDesc.Count = 1;
+		resourceDescriptor.SampleDesc.Quality = 0;
+		resourceDescriptor.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+		resourceDescriptor.Flags = D3D12_RESOURCE_FLAG_NONE;
+
+		//Creating GPU memory pointer
+		ComPointer<ID3D12Resource> uploadBuffer, vertexBuffer;
+		DXContext::Get().GetDevice()->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &resourceDescriptor, D3D12_RESOURCE_STATE_COMMON, nullptr, IID_PPV_ARGS(&uploadBuffer));
+		DXContext::Get().GetDevice()->CreateCommittedResource(&defaultHeapProperties, D3D12_HEAP_FLAG_NONE, &resourceDescriptor, D3D12_RESOURCE_STATE_COMMON, nullptr, IID_PPV_ARGS(&vertexBuffer));
+
+		// Copy void* --> CPU Resource
+		void* uploadBufferAdress;
+		D3D12_RANGE uploadRange;
+		uploadRange.Begin = 0;
+		uploadRange.End = 1023;
+		uploadBuffer->Map(0, &uploadRange, &uploadBufferAdress);
+		memcpy(uploadBufferAdress, hello, strlen(hello) + 1);
+		uploadBuffer->Unmap(0, &uploadRange);
+
+		// Async Copy CPU Resource --> GPU Resource
+		auto* cmdList = DXContext::Get().InitCommandList();
+		cmdList->CopyBufferRegion(vertexBuffer, 0, uploadBuffer, 0, 1024);
+
+		DXContext::Get().ExecuteCommandList();
+
+		DXWindow::Get().SetFullscreen(true);
 		while (!DXWindow::Get().ShouldClose())
 		{
 			// Process pending window messages
@@ -30,7 +81,7 @@ int main()
 			}
 
 			//Begin drawing
-			auto* cmdList = DXContext::Get().InitCommandList();
+			cmdList = DXContext::Get().InitCommandList();
 
 			// Draw to window
 			DXWindow::Get().BeginFrame(cmdList);		
